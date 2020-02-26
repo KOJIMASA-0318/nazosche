f4edacad3927654616202eb72f55b9299a2af5af
KOJIMASA-0318 <masayuki.k.2020@gmail.com>
2020-02-26 18:07:50 +0900

    eventAndNavbar0226 #10 #11 #25

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  # 選択中のサイドメニューのクラスを返す
+  def sidebar_activate(sidebar_link_url)
+    current_url = request.path
+    if current_url.match(sidebar_link_url)
+      ' class="active"'
+    else
+      ''
+    end
+  end
+
+  # サイドメニューの項目を出力する
+  def sidebar_list_items
+    items = [
+      {:text => 'カレンダー',   :path => user_events_path(current_user)},
+      {:text => '予定を追加する',   :path => new_user_event_path(current_user)},
+      {:text => '参加予定のイベント',   :path => user_upcoming_path(current_user)},
+      {:text => '参加済のイベント',   :path => user_past_path(current_user)}
+    ]
+
+    html = ''
+    items.each do |item|
+      text = item[:text]
+      path = item[:path]
+      html = html + %Q(<li#{sidebar_activate(path)}><a href="#{path}">#{text}</a></li>)
+    end
+
+    raw(html)
+  end
 end
