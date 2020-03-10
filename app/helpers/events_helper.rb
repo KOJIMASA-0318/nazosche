11fc7d9ae9392e608347b48372870ef7266b8429
KOJIMASA-0318 <masayuki.k.2020@gmail.com>
2020-03-10 18:57:51 +0900

    bugFix0310

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def result_ja(result)
+    case result
+    when "upcoming"
+      "開催前"
+    when "success"
+      "成功"
+    when "failure"
+      "失敗"
+    when "joined"
+      "参加済み"
+    end
+  end
 end
